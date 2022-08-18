6c6f2cc68eece651a66dbbf074d87c0cd3e460b5
nigesh <nigesh@newagesmb.com>
2022-08-18 17:36:33 +0530

    Initial Commit

---
+++ Reiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reiki
+//
+//  Created by NewAgeSMB on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
