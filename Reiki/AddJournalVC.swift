e3de83e9bbf014a281289396974b8355fab12698
nigeshvenu <nigesh@newagesmb.com>
2024-04-10 11:09:29 +0530

    Animation Feature - Dashboard

---
+++ Reiki/AddJournalVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddJournalVC.swift
+//  Reiki
+//
+//  Created by NewAgeSMB on 28/09/22.
+//
+
+import UIKit
+
+class AddJournalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
