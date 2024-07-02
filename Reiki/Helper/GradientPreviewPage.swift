7be766a69d42b90e22f6cd0d4f50d82fdbbf4f70
nigeshvenu <nigesh@newagesmb.com>
2024-07-02 15:20:28 +0530

    Edit profile Change avatar

---
+++ Reiki/Helper/GradientPreviewPage.swift
@@ -0,0 +1,20 @@
+//
+//  GradientPreviewPage.swift
+//  Reiki
+//
+//  Created by Newage on 16/04/24.
+//
+
+import UIKit
+
+class GradientPreviewPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
